b233542b1d697521d4d954424953737a0c9e3fa3
Brenda Brizola <oliveira.brenda23@edu.pucrs.br>
2024-10-21 16:48:18 -0300

    criado o package de aplicação, começei a fazer as DTOs, ficou faltando terminar elas e começar as Use Cases

---
+++ src/main/java/ProjArq/ControlSubApp/aplicacao/dtos/ClienteDTO.java
@@ -0,0 +1,31 @@
+package ProjArq.ControlSubApp.aplicacao.dtos;
+
+public class ClienteDTO {
+    private long codigo;
+    private String nome;
+    private String email;
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
