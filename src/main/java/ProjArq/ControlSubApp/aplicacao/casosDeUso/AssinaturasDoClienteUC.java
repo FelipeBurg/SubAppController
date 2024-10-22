67acf5f1830aa4778f923b98bee14bbc83e83a47
Felipe Burgdurf <96593150+FelipeBurg@users.noreply.github.com>
2024-10-22 17:04:07 +0000

    "Criado os useCases, falta dois"

---
+++ src/main/java/ProjArq/ControlSubApp/aplicacao/casosDeUso/AssinaturasDoClienteUC.java
@@ -0,0 +1,19 @@
+import ProjArq.ControlSubApp.interfaceAdaptadora.repositorios.Repositories.AssinaturaRepository;
+import ProjArq.ControlSubApp.domain.entidades.Assinatura;
+
+
+public class AssinaturasDoClienteUC{
+
+    private final AssinaturaRepository assinaturaRepository;
+
+    public AssinaturasDoClienteUC (AssinaturaRepository assinaturaRepository){
+        this.assinaturaRepository = assinaturaRepository;
+    }
+
+    public List<Assinatura> execute(long clienteCodigo) {
+        return assinaturaRepository.findA(clienteCodigo);
+    }
+
+
+
+}
